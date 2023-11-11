c5e58ac4a0c1ea072029afd8bb1d695ba8db6110
cidesgo@gmail.com <cidesgo@gmail.com>
2023-11-10 23:21:22 -0600

    Tercer commit autenticacion

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import {StyleSheet, View, Text } from 'react-native';
 import {PaperProvider} from 'react-native-paper';
+import Prueba from './src/components/Prueba';
+import { useState } from 'react';
+import Auth from './src/screens/auth';
 
 export default function App() {
+  const [auth, setAuth] = useState(undefined)
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>Mi segunda app</Text>
+        {auth ? <Text>Zona De Usuario</Text> : <Auth/>}
       </View>
     </PaperProvider>
   );
