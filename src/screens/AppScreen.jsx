2fc8350074b38c4238df33f7ea12e71c872d8929
cidesgo@gmail.com <cidesgo@gmail.com>
2023-12-02 00:10:37 -0600

    aplicacion todoList y RegisterLogin Unidas

--- src/screens/AppScreen.jsx
+++ src/screens/AppScreen.jsx
@@ -21,104 +21,99 @@ export default function AppScreen() {
   };
 
   return (
-    <View>
-      <Text>HOLA ESTAS EN TODO</Text>
-    </View>
+    <View style={styles.container}>
 
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1
+      }}
+      keyboardShouldPersistTaps='handled'
+    >
 
-  //   <View style={styles.container}>
+      {/* Mostrar las tareas */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>
+          Today's Tasks
+        </Text>
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+              return(
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item}/>
+                </TouchableOpacity>
+              );
+            })
+          }
+        </View>
+      </View>
+    </ScrollView>
 
-  //   <ScrollView
-  //     contentContainerStyle={{
-  //       flexGrow: 1
-  //     }}
-  //     keyboardShouldPersistTaps='handled'
-  //   >
+    {/* Escribir una nueva tarea*/}
+    <KeyboardAvoidingView
+     behavior={Platform.OS === "ios" ? "padding" : "height" }
+     style={styles.writeTaskWrapper}>
 
-  //     {/* Mostrar las tareas */}
-  //     <View style={styles.tasksWrapper}>
-  //       <Text style={styles.sectionTitle}>
-  //         Today's Tasks
-  //       </Text>
-  //       <View style={styles.items}>
-  //         {
-  //           taskItems.map((item, index) => {
-  //             return(
-  //               <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-  //                 <Task text={item}/>
-  //               </TouchableOpacity>
-  //             );
-  //           })
-  //         }
-  //       </View>
-  //     </View>
-  //   </ScrollView>
+      <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
 
-  //   {/* Escribir una nueva tarea*/}
-  //   <KeyboardAvoidingView
-  //    behavior={Platform.OS === "ios" ? "padding" : "height" }
-  //    style={styles.writeTaskWrapper}>
+      <TouchableOpacity onPress={() => handleAddTask()} >
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
 
-  //     <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
-
-  //     <TouchableOpacity onPress={() => handleAddTask()} >
-  //       <View style={styles.addWrapper}>
-  //         <Text style={styles.addText}>+</Text>
-  //       </View>
-  //     </TouchableOpacity>
-  //   </KeyboardAvoidingView>
-
-  // </View>
+  </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#604848',
-//   },
-//   tasksWrapper: {
-//     paddingTop: 80,
-//     paddingHorizontal: 20,
-//   },
-//   sectionTitle:{
-//     fontWeight: 'bold',
-//     fontSize: 30,
-//     marginBottom: 35
-//   },
-//   items:{
-//     marginTop: 10
-//   },
-//   writeTaskWrapper:{
-//     position: 'absolute',
-//     bottom: 70,
-//     width: '100%',
-//     flexDirection: 'row',
-//     justifyContent: 'space-around',
-//     alignItems: 'center',
-//   },
-//   input:{
-//     paddingVertical: 15,
-//     paddingHorizontal: 15,
-//     backgroundColor: "#789048",
-//     borderRadius: 60,
-//     borderColor: '#607848',
-//     borderWidth: 2,
-//     width: 250
-//   },
-//   addWrapper:{
-//     width: 50,
-//     height: 50,
-//     backgroundColor: "#789048",
-//     borderRadius: 50,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     borderColor: '#607848',
-//     borderWidth: 2,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'papayawhip',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: 35
+  },
+  items:{
+    marginTop: 10
+  },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 70,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#789048",
+    borderRadius: 60,
+    borderColor: '#607848',
+    borderWidth: 2,
+    width: 250
+  },
+  addWrapper:{
+    width: 50,
+    height: 50,
+    backgroundColor: "#789048",
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#607848',
+    borderWidth: 2,
 
-//   },
-//   addText:{
-//     fontSize: 30,
-//     color: "#604848"
-//   }
-// });
+  },
+  addText:{
+    fontSize: 30,
+    color: "#604848"
+  }
+});
